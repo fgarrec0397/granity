bbecefd6da5564bb928debdf937e71c42ccd0e43
fga97 <fgarrec0397@gmail.com>
2022-05-07 20:17:31 -0400

    Separation of server and app

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./App/App";
 import "./index.css";
 import "antd/dist/antd.css";
 import AppProviders from "./App/AppProviders";
 
-ReactDOM.render(
+render(
     <React.StrictMode>
         <AppProviders>
             <App />
