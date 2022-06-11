8a451541aa3025718d81a2fdc80a0ceb5b68ef40
fga97 <fgarrec0397@gmail.com>
2022-06-11 14:00:44 -0400

    Removed GameCamera component in widget

--- app/src/Features/Widgets/Cameras/Cameras.tsx
+++ app/src/Features/Widgets/Cameras/Cameras.tsx
@@ -1,14 +1,33 @@
-import { FC } from "react";
+import { useHelper } from "@react-three/drei";
+import { FC, useEffect, useRef } from "react";
+import { Camera } from "@react-three/fiber";
+import { CameraHelper } from "three";
 import { EditableWidget } from "../../../App/Editor/types";
+import useCameras from "../../../App/Scene/hooks/useCameras";
 import { WidgetModule } from "../../../App/Widgets/types";
-import GameCamera from "../../Components/Cameras/GameCamera";
 
 export type CamerasProps = EditableWidget;
 
 type OwnProps = CamerasProps;
 
 const Cameras: FC<OwnProps> = () => {
-    return <GameCamera />;
+    const { addCamera } = useCameras();
+    const cameraRef = useRef<Camera>(null!);
+
+    useEffect(() => {
+        addCamera({ cameraRef });
+    }, [addCamera]);
+
+    useHelper(cameraRef, CameraHelper);
+
+    return (
+        <>
+            <mesh scale={[0.25, 0.25, 0.25]}>
+                <boxGeometry />
+                <perspectiveCamera ref={cameraRef} />
+            </mesh>
+        </>
+    );
 };
 
 export const widget: WidgetModule<CamerasProps> = {
