39aeb2dbceb385f4e9e93b450b628ccc8dd1c4e5
fga97 <fgarrec0397@gmail.com>
2022-07-08 13:35:48 -0400

    Commenting complex code

--- app/src/App/Scene/_actions/hooks/useHandleGetScene.ts
+++ app/src/App/Scene/_actions/hooks/useHandleGetScene.ts
@@ -23,10 +23,12 @@ export default () => {
                     ];
                     const sceneWidgetOptions = [...(x?.widgetDefinition?.options || [])];
 
+                    // Replace saved widget definitions options with local widget definitions options
                     if (sceneWidgetOptions.length && widgetModuleOptions.length) {
                         x.widgetDefinition.options = widgetModuleOptions;
                     }
 
+                    // Unserialize meshHolder component
                     if (x.editorOptions?.meshHolder) {
                         x.editorOptions.meshHolder = deserialize(
                             x.editorOptions.meshHolder as string
@@ -39,22 +41,24 @@ export default () => {
                     } as WidgetSceneObject;
                 });
 
-                // TODO -- Break this down into reusable code
-
                 const mergedWidgetDictionary = buildWidgetsDictionary(deserializedWidgets);
 
+                // Merge saved widget dictionary based on current widgets definitions options
                 Object.keys(mergedWidgetDictionary).forEach((dictionaryItemKey) => {
                     const dictionaryItem = mergedWidgetDictionary[dictionaryItemKey];
 
                     for (const key in data.widgetsDictionary[dictionaryItemKey].options) {
                         if (!Object.prototype.hasOwnProperty.call(dictionaryItem.options, key)) {
+                            // Remove unexisting options on the local widget definitions options
                             delete data.widgetsDictionary[dictionaryItemKey].options[key];
                         }
                     }
 
+                    // Make sure to keep the options left from the saved widget dictionary
                     mergedWidgetDictionary[dictionaryItemKey].options =
                         data.widgetsDictionary[dictionaryItemKey].options;
 
+                    // Make sure to keep the properties left from the saved widget dictionary
                     mergedWidgetDictionary[dictionaryItemKey].properties =
                         data.widgetsDictionary[dictionaryItemKey].properties;
                 });
