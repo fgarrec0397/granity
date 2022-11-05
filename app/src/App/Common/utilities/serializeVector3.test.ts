23c0ca2fbee43db8eb658c50b18f5e5d76412707
fga97 <fgarrec0397@gmail.com>
2022-11-05 14:03:20 -0400

    tests moved into a test folder relative to the function folder

--- app/src/App/Common/utilities/serializeVector3.test.ts
+++
@@ -1,9 +0,0 @@
-import { Vector3 } from "three/src/Three";
-
-import serializeVector3 from "./serializeVector3";
-
-describe("Serialize a Vector3 into an array of number representing the X, Y and Z coordinate", () => {
-    test("adds 1 + 2 to equal 3", () => {
-        expect(serializeVector3(new Vector3(0, 0, 0))).toStrictEqual([0, 0, 0]);
-    });
-});
