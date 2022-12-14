a95ac036deeea93c70e525ca3111aaac1ded53c4
fga97 <fgarrec0397@gmail.com>
2022-12-13 20:56:23 -0500

    refactor: minor changes

--- app/src/Themes/globalStyle.ts
+++ app/src/Themes/globalStyle.ts
@@ -8,6 +8,19 @@ export default createGlobalStyle`
 
 	@import url(${({ theme }) => theme.typography.fontFamilyURL});
 
+	* {
+    box-sizing: border-box;
+	}
+	
+	html,
+	body,
+	#root {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+
 	body {
 		font-family: ${({ theme }) => theme.typography.fontFamily.main};
 		font-size: ${rootFontSize}px;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,7 +4,7 @@ import "antd/dist/antd.css";
 import AppProvider from "@app/Core/_actions/_data/providers/AppProvider";
 import Core from "@app/Core/Core";
 import FeaturesProvider from "@features/Core/_actions/_data/providers/FeaturesProvider";
-import GlobalStyle from "@themes/GlobalStyle";
+import GlobalStyle from "@themes/globalStyle";
 import ThemeProvider from "@themes/ThemeProvider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
