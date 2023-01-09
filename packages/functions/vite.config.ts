5dbe479413a81be5f55041cf12cc362c32d7a07e
fga97 <fgarrec0397@gmail.com>
2023-01-08 19:51:37 -0500

    test: setup for the whole monorepo

---
+++ packages/functions/vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+            reporter: ["text", "html"],
+            exclude: ["node_modules/", "src/setupTests.ts"],
+        },
+    },
+});
