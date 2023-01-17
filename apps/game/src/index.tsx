252fc00208a453fcb760454de19de97f4c05a096
fga97 <fgarrec0397@gmail.com>
2023-01-16 20:09:06 -0500

    chore: game package init

---
+++ apps/game/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>hello world</React.StrictMode>
+);
