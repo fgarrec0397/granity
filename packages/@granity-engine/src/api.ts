a32fa767669b7809339505fe4fc5e16f766fcd1c
fga97 <fgarrec0397@gmail.com>
2023-01-30 20:35:46 -0500

    refactor: engine renamed to admin

--- packages/@granity-engine/src/api.ts
+++ packages/@granity-engine/src/api.ts
@@ -3,6 +3,5 @@ export * from "./App/Editor";
 export * from "./App/Game";
 export * from "./App/Scenes";
 export * from "./App/Widgets";
-// export type { EngineConfig as GranityEngineConfig } from "./index";
 export { GranityEngine } from "./index";
 export { default as GranityEngineProvider } from "./Providers";
