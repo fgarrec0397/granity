3423d7c6ad05eaaa2d38781b84544eaa02cfee11
fga97 <fgarrec0397@gmail.com>
2023-02-07 21:08:47 -0500

    fix: box background props

--- packages/@granity-ui/src/Components/Atoms/Box/Box.tsx
+++ packages/@granity-ui/src/Components/Atoms/Box/Box.tsx
@@ -1,10 +1,17 @@
+import { styled } from "@mui/material";
 import BoxLib, { BoxProps as LibBoxProps } from "@mui/material/Box";
 import { FC } from "react";
 
-export type BoxProps = LibBoxProps;
+export type BoxProps = LibBoxProps & {
+    hasBackground?: boolean;
+};
 
-const Box: FC<BoxProps> = (props) => {
-    return <BoxLib {...props} />;
+const StyledBox = styled(BoxLib)<BoxProps>(({ hasBackground, theme }) => ({
+    backgroundColor: hasBackground ? theme.palette.background.paper : undefined,
+}));
+
+const Box: FC<BoxProps> = ({ hasBackground = true, ...props }) => {
+    return <StyledBox hasBackground={hasBackground} {...props} />;
 };
 
 export default Box;
