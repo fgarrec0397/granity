4edab2e08fa7e1f099d05ae009f26fac34cdab28
fga97 <fgarrec0397@gmail.com>
2023-03-15 20:18:22 -0400

    chore: transpilePAckages removed from experimental

--- apps/admin-dashboard/next.config.js
+++ apps/admin-dashboard/next.config.js
@@ -2,30 +2,16 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    transpilePackages: [
+        "@granity/engine",
+        "@granity/eslint-config",
+        "@granity/helpers",
+        "@granity/prettier-config",
+        "@granity/ui",
+        "@granity/widgets",
+    ],
     experimental: {
         appDir: true,
-        transpilePackages: [
-            "@granity/engine",
-            "@granity/eslint-config",
-            "@granity/helpers",
-            "@granity/prettier-config",
-            "@granity/ui",
-            "@granity/widgets",
-        ],
-        // modularizeImports: {
-        //     lodash: {
-        //         transform: "lodash/{{member}}",
-        //     },
-        //     "@mui/material": {
-        //         transform: "@mui/material/{{member}}",
-        //     },
-        //     "@mui/lab": {
-        //         transform: "@mui/lab/{{member}}",
-        //     },
-        //     "@mui/icons-material/?(((\\w*)?/?)*)": {
-        //         transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
-        //     },
-        // },
     },
     async rewrites() {
         return [
