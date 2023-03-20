611f3715ae8f742bf407a64c4e45a6e84279765c
fga97 <fgarrec0397@gmail.com>
2023-03-20 19:27:05 -0400

    chore: added bundle analyser for webpack + sideeffects to false

--- apps/admin-dashboard/next.config.js
+++ apps/admin-dashboard/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
@@ -22,6 +27,10 @@ const nextConfig = {
             },
         ];
     },
+    webpack: (config) => ({
+        ...config,
+        mode: "production",
+    }),
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
